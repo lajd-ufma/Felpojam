b710b40fe8d46f32741d55df186cbe88fc50fdde
Flávio Guilherme Santana <guilher95161@gmail.com>
2026-02-23 19:56:03 -0300

    Mudanças telas e criação pause_menu
    Criação:
    1 - Criação da tela de pause
    2 - Criação do shader da tela de pause
    3 - Criação de um botão genérico para representar todos os botões que possam aparecer
    Mudanças:
    1 - Mudanças no levels para ter o menu de pause
    2 - Mudanças na tela de créditos para ser responsiva
    3 - Mudanças na tela de título por causa da referência de certos arquivos que mudaram de pasta pela refatoração dos caminhos feitos

--- menus/scripts/tittle_screen.gd
+++ menus/scripts/tittle_screen.gd
@@ -1,11 +1,11 @@
 extends Control
 
 func _on_start_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/playground.tscn")
+	get_tree().change_scene_to_file("res://levels/level1.tscn")
 
 
 func _on_credits_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/credits_screen.tscn")
+	get_tree().change_scene_to_file("res://menus/scripts/credits_screen.gd")
 
 
 func _on_quit_btn_pressed() -> void:
