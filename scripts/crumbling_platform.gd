96b0c74ce2f9e227dc3e3a9f8e7827de15d3e041
pedenrique9029 <pedrommfrv@gmail.com>
2026-02-26 13:26:51 -0300

    wip: platforms (crumbling sem tremor e reverse crumbling sem textura  própria)

--- scripts/crumbling_platform.gd
+++ scripts/crumbling_platform.gd
@@ -1,15 +1,19 @@
 extends MovingPlatform
 
 @onready var timer_fall: Timer = $TimerFall
+@onready var animation_player_shake: AnimationPlayer = $AnimatableBody2D/AnimationPlayer
 
 var is_carimbado := false
 
 func _on_detect_player_body_entered(body: Node2D) -> void:
+	animation_player_shake.play("shake")
+	print(animation_player_shake.current_animation)
 	if !manchou.is_connected(_stop_fall):
 		manchou.connect(_stop_fall)
 	timer_fall.start()
 func _stop_fall():
 	is_carimbado = true
+	animation_player_shake.stop()
 	timer_fall.stop()
 func _on_timer_fall_timeout() -> void:
 	if !is_carimbado:
