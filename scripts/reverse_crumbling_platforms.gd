9234d2786ad6e53994eb1e3fc7350c4bfc23cb63
Flávio Guilherme Santana <guilher95161@gmail.com>
2026-02-26 16:27:33 -0300

    plataforma reversa caindo wow
    sasas

--- scripts/reverse_crumbling_platforms.gd
+++ scripts/reverse_crumbling_platforms.gd
@@ -1,7 +1,34 @@
 extends MovingPlatform
 
+@onready var timer_fall: Timer = $TimerFall
+@onready var anim: AnimationPlayer = $AnimatableBody2D/AnimationPlayer
+
+var is_falling := false
+var fall_velocity := Vector2.ZERO
+var fall_gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _ready() -> void:
 	manchou.connect(_fall)
 
 func _fall():
 	queue_free()
+
+
+func _on_detect_player_body_entered(body: Node2D) -> void:
+	if timer_fall.is_stopped() and !is_falling:
+		timer_fall.start()
+
+
+func _on_timer_fall_timeout() -> void:
+	is_falling = true
+
+
+func _physics_process(delta: float):
+	if is_falling:
+		fall_velocity.y += fall_gravity * delta
+		platform.position += fall_velocity * delta
+		
+		if platform.global_position.y > 2500:
+			queue_free()
+	else:
+		super._physics_process(delta)
